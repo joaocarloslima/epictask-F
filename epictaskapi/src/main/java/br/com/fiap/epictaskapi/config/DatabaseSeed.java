891816bfa9f57996b55b70bc1c94ffadd88f77d5
Joao <joaocarloslima@me.com>
2022-08-25 12:04:29 -0300

    spring ops

--- epictaskapi/src/main/java/br/com/fiap/epictaskapi/config/DatabaseSeed.java
+++ epictaskapi/src/main/java/br/com/fiap/epictaskapi/config/DatabaseSeed.java
@@ -20,7 +20,13 @@ public class DatabaseSeed implements CommandLineRunner {
 
         repository.saveAll(List.of(
             new Task("Modelar BD", "modelar tabelas do banco", 150),
-            new Task("Prototipo", "prototipar as telas", 250),
+            new Task("Prototipo", "prototipar as telas", 20),
+            new Task("Login", "prototipar as telas", 10),
+            new Task("Deploy", "prototipar as telas", 50),
+            new Task("Cadastro de usuário", "prototipar as telas", 30),
+            new Task("Testes", "prototipar as telas", 25),
+            new Task("Logout", "prototipar as telas", 12),
+            new Task("Internacionalização", "prototipar as telas", 70),
             new Task("Bug", "corrigir erro da API", 50)
         ));
     }
